85213d78b7fcc4bde07ebc2f39df79a8864662cf
Ari Vuorenmaa <ari.vuorenmaa@gmail.com>
2014-01-30 20:04:16 +0800

    add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Jooga App!"
+      flash[:success] = "Welcome, you are now user of Merseri!"
       redirect_to @user
     else
       render 'new'
