2b1c4ef989892e683e8de36bab3e189e2cba6248
Ari Vuorenmaa <ari.vuorenmaa@gmail.com>
2014-02-03 18:36:28 +0800

    add cleaning

--- app/models/mer.rb
+++ app/models/mer.rb
@@ -5,9 +5,8 @@ class Mer < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   
-  validates :title, presence: true,
-                    length: { minimum: 1, maximum: 255 }
-                    
+  validates :title, :presence => {:message => 'Idea cannot be blank'}, :length => { :minimum => 3 }
+                   
   def self.search(query)
    # where(:title, query) -> This would return an exact match of the query
    where("title like ?", "%#{query}%") 
