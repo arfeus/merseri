80c062f5292088af8ad835a4fcc79bb3d4183c0f
Ari Vuorenmaa <ari.vuorenmaa@gmail.com>
2014-02-20 15:37:31 +0800

    add bootstrap3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome, you are now user of Merseri!"
+      flash[:notice] = "Welcome, you are now user of Merseri!"
       redirect_to newuser_path
     else
       render 'new'
