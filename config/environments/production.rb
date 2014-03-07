9e979947d22c15ea8b53985196426bd81c4c34bb
Ari Vuorenmaa <ari.vuorenmaa@gmail.com>
2014-03-07 16:11:53 +0800

    add welcome mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,18 +79,29 @@ Merseri::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   
   # Action_mailer configuration for production
-  ##config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = true
   
-  #config.action_mailer.smtp_settings = {
+ # config.action_mailer.smtp_settings = {
     
-  #  address:              'smtp.gmail.com',
-  #  port:                 587,
-  #  domain:               'gmail.com',
-  #  user_name:            'simo.siiseli@gmail.com',
-  #  password:             'hallinto011',
-  #  authentication:       'plain',
-  #  enable_starttls_auto: true  }
+ #   address:              'smtp.sendgrid.net',
+ #   port:                 587,
+ #   domain:               'merseri.herokuapp.com',
+ #   user_name:            'app21210950@heroku.com',
+ #   password:             'epwo6moj',
+ #   authentication:       'plain',
+ #   enable_starttls_auto: true  }
+  
+    ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
+  
   
 end
