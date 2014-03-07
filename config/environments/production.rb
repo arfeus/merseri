62636e88a63ccd98286fb7a740381495d47e9f8d
Ari Vuorenmaa <ari.vuorenmaa@gmail.com>
2014-03-07 13:33:25 +0800

    add welcome mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,19 +78,6 @@ Merseri::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  # Action_mailer configuration for production
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  
-  config.action_mailer.smtp_settings = {
-    
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'simo.siiseli@gmail.com',
-    password:             'hallinto010',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+
   
 end
