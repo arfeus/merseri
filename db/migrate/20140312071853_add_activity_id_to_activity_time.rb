dcf6383fb090b6049f111206795259c1306b97d9
Ari Vuorenmaa <ari.vuorenmaa@gmail.com>
2014-03-14 20:52:52 +0800

    drop activity_times table

--- db/migrate/20140312071853_add_activity_id_to_activity_time.rb
+++
@@ -1,5 +0,0 @@
-class AddActivityIdToActivityTime < ActiveRecord::Migration
-  def change
-    add_column :activity_times, :activity_id, :integer
-  end
-end
--- db/migrate/20140312072238_add_index_to_activity_times_activity_id.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToActivityTimesActivityId < ActiveRecord::Migration
-  def change
-    add_index :activity_times, :activity_id
-  end
-end
